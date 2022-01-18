91703fc8e8e609320ed54f9b9161c6ca56387c31
Peter <peter.tveit@hotmail.com>
2022-01-18 16:21:16 +0700

    Update path

--- 2-semester/Objektorientert programmering/step-4/step-4/App/src/Program.java
+++ 2-semester/Objektorientert programmering/step-4/step-4/App/src/Program.java
@@ -44,44 +44,50 @@ public class Program {
     }
 
     public void task7() {
-        System.out.println("Select:");
-        System.out.println("1. Add a single word");
-        System.out.println("2. Display all words");
-        System.out.println("3. End");
-
         ArrayList<String> arrayWords = new ArrayList<String>();
         
-        Scanner input = new Scanner(System.in);
-
-        int userInput = input.nextInt();
-
         class run {
+            public void initiate() {
+                System.out.println();
+                System.out.println("Select:");
+                System.out.println("1. Add a single word");
+                System.out.println("2. Display all words");
+                System.out.println("3. End");
+                
+                try (Scanner input = new Scanner(System.in)) {
+                    int userInput = input.nextInt();
+                    
+                    if (userInput == 1) {
+                        new run().addWord();
+                    } else if (userInput == 2){
+                        new run().displayWords();
+                    } else if (userInput == 3) {
+                        new run().end();
+                    }
+                }
+            }
             
             public void addWord() {
                 System.out.println();
                 System.out.println("Add a word");
-                
+                Scanner input = new Scanner(System.in);
                 String userInput = input.nextLine();
                 arrayWords.add(userInput);
-                task7();
+                initiate();
             }
             
             public void displayWords() {
-                
+                System.out.println();
+                System.out.println(arrayWords);
+                initiate();
             }
             
             public void end() {
-                
+                System.out.println();
+                System.out.println("Goodbye :)");
             }
         }
-        
-        if (userInput == 1) {
-            new run().addWord();
-        } else if (userInput == 2){
-            new run().displayWords();
-        } else if (userInput == 3) {
-            new run().end();
-        }
-        input.close();
+
+        new run().initiate();
     }
 }
